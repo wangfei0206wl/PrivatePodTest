4faf746902f2c53e022d5a9e2ae3b258d193b854
wangfei_mac <wangfei_mac@wangfeideMacBook-Pro-2.local>
2018-11-29 11:46:02 +0800

    add file

---
+++ CocoapodDemo/hotfix_bug.m
@@ -0,0 +1,7 @@
+// xell
+http://www.meijuck.com/lyjs/15202.html
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+return [target performSelector:action withObject:params];
+#pragma clang diagnostic pop
